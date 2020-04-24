b66071e022d9d8261ea916bc3647e61f4dcb37ed
DanyeDarko <danielbrzen@outlook.com>
2020-04-24 11:59:19 -0500

    CAMBIO DE NOMBRE DE 'app' a 'run'

--- flask/app.py
+++
@@ -1,4 +0,0 @@
-from app import app
-
-if __name__ == '__main__':
-    app.run(debug=True,host='192.168.1.100',port=2000)
---
+++ flask/run.py
@@ -0,0 +1,6 @@
+from app import app
+# DESDE LA CARPETA 'app' importa el modulo app
+
+if __name__ == '__main__':
+    app.run()
+    
