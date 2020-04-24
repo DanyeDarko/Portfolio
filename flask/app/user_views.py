4634b40f25fd4ed12fb6111e9a781145b003910e
DanyeDarko <danielbrzen@outlook.com>
2020-04-24 12:03:18 -0500

    VISTAS DE USUARIO COMUN

--- flask/app/user_views.py
+++ flask/app/user_views.py
@@ -4,4 +4,4 @@ from flask import render_template
 @app.route('/')
 def index():
    user = {'Nombre':'Daniel','Apellido':'Brito','Score':50}
-   return render_template("public/index.html", result =  user)
+   return render_template("public/user_dashboard.html", result =  user)
