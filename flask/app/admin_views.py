8be91af976bc8e49eacab3e2f7ebad03a6130f4e
DanyeDarko <danielbrzen@outlook.com>
2020-04-24 12:03:30 -0500

    VISTAS DE USUARIO ADMINISTRADOR

--- flask/app/admin_views.py
+++ flask/app/admin_views.py
@@ -1,8 +1,9 @@
 from app import app 
 from flask import render_template
 
-
 @app.route('/admin/dashboard')
 def admin_dashboard():
    dict = {'Nombre':'Daniel','Apellido':'Brito','Score':50}
-   return render_template("admin/dashboard.html",result = dict)
+   return render_template("admin/admin_dashboard.html",result = dict)
+
+
