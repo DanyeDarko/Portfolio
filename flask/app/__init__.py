5a58058134de913fe5659dc51061a5a43d63ade0
DanyeDarko <danielbrzen@outlook.com>
2020-04-24 12:04:56 -0500

    AGREGAMOS SECRET_KEY Y CAMBIAMOS NOMBRE DE RUTAS

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,9 +1,13 @@
-# constructor de la aplicacion 
-
+# CONSTRUCTOR DE app.py
 from flask import Flask
 # MICROFRAMEWORK FLASK PARA PRESENTAR API'S Y RENDERISAR TEMPLATES
+
 app = Flask(__name__)
 # NUEVA ESTANCIA DEL MODULO 'FLASK' LLAMADA 'app'
 
-from app import views,admin_views
-# DEL MODULO 'app' (DIRECTORIO) IMPORTAMOS LAS VISTAS 
+app.config.from_mapping(
+        SECRET_KEY='dev',
+)
+
+from app import user_views,admin_views
+# DEL MODULO 'app' (DIRECTORIO) IMPORTAMOS LAS VISTAS DEL USUARIO Y DEL ADMINISTRADOR
