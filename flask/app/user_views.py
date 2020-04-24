499efb7a72fd2ed5fadae166aecef204178801c6
DanyeDarko <danielbrzen@outlook.com>
2020-04-24 13:57:33 -0500

    AGREGAMOS EL uso de 'dotenv' para var de entorno

--- flask/app/user_views.py
+++ flask/app/user_views.py
@@ -1,7 +1,19 @@
 from app import app 
 from flask import render_template
+from dotenv import load_dotenv
+import os 
+
 
 @app.route('/')
-def index():
+def index_login():
+   load_dotenv()
+   OS = os.getenv('OS')  
+   passw = os.getenv('PASS_SQL') 
+   return render_template('auth/login.html',password=passw)
+
+@app.route('/dashboard')
+def user_dashboard():
    user = {'Nombre':'Daniel','Apellido':'Brito','Score':50}
-   return render_template("public/user_dashboard.html", result =  user)
+   return render_template('public/user_dashboard.html', result =  user)
+
+
