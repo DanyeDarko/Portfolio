1d6d51ac0447fed95d9ea568c840040b0f45807c
DanyeDarko <danielbrzen@outlook.com>
2020-04-24 13:57:05 -0500

    adicion de ruta para modificar perfil

--- flask/app/admin_views.py
+++ flask/app/admin_views.py
@@ -1,4 +1,5 @@
 from app import app 
+
 from flask import render_template
 
 @app.route('/admin/dashboard')
@@ -6,4 +7,7 @@ def admin_dashboard():
    dict = {'Nombre':'Daniel','Apellido':'Brito','Score':50}
    return render_template("admin/admin_dashboard.html",result = dict)
 
+@app.route('/admin/perfil')
+def admin_profile():
+    return 'PERFIL USUARIO'
 
