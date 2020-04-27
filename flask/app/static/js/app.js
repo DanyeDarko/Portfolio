ed41608fabc2d849ace976440b7fd030108ca9ff
DanyeDarko <danielbrzen@outlook.com>
2020-04-27 14:30:09 -0500

    agregamos codigo Jquery para menu y modal

--- flask/app/static/js/app.js
+++ flask/app/static/js/app.js
@@ -1,5 +1,17 @@
 console.log("HELLO FROM APP FLASK JS")
 
+// FUNCION DE MENU SUPERIOR PARA MENU SANDWICH
+
 $("document").ready(function(){
     $(".button-collapse").sideNav();
   });
+
+// FUNCION DE MODAL PARA CONFIGURACION
+$("document").ready(function(){
+  $('.modal').modal();
+});
+
+
+$(".dropdown-button").dropdown({ hover: false } );
+
+        
