416ef2cba20864e070cf2155788b64f1d509fd31
DanyeDarko <danielbrzen@outlook.com>
2020-04-28 12:56:47 -0500

    pruebas para Jinja2 y renderisar datos template

--- flask/app/admin_views.py
+++ flask/app/admin_views.py
@@ -4,8 +4,12 @@ from flask import render_template
 
 @app.route('/admin/dashboard')
 def admin_dashboard():
-   dict = {'Nombre':'Daniel','Apellido':'Brito','Score':50}
-   return render_template("admin/admin_dashboard.html",result = dict)
+   data = []
+   user_data ={'nombre_user':'Jayr','edad':27,'tipo_user':'admin','correo':'jayrmz@realdental.com'}
+   consult_data ={'ubicacion':'1223','nombre':'Real Dental','numero_citas':5}
+   data.append(user_data)
+   data.append(consult_data)
+   return render_template("admin/admin_dashboard.html",data = data )
 
 @app.route('/admin/perfil')
 def admin_profile():
